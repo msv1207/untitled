c72ae8a9207db366459d5ded254daf3201da7274
Misha Vlasov <mishavlasov@MacBook-Air-Misha.local>
2021-06-13 11:09:35 +0300

    Initial commit

---
+++ 1.js
@@ -0,0 +1,85 @@
+// let a=3
+// alert(a);
+// let a=10, b=2
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// alert(a/b);
+// let c=15, d=2, rusult
+// result=c+d;
+// alert(result)
+// let  a=10, b=2, c=5
+// alert(a+b+c)
+// let a=17, b=10
+// let c=a-b, d=7
+// let result=c+d
+// alert(result)
+// let text='Привет, Мир!'
+// alert(text)
+// let text1='hello,', text2='world!'
+// alert(text1 + text2)
+// alert(60 * 60);
+// alert(60 * 60 * 24);
+// alert(60 * 60 * 24 * 30);
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7;
+// num += 1;
+// num -= 1;
+// console.log(num)
+// let
+//
+//     now = new Date(),
+//     hour = now.getHours(),
+//     minute = now.getMinutes(),
+//     second = now.getSeconds();
+// alert(`${hour}:${minute}:${second}`)
+// let text
+// text = 'Я'
+// text +=' хочу'
+// text +=' знать'
+// text += ' JS!'
+// console.log(text)
+// let foo = 'bar', bar=10
+// console.log(eval(foo))
+// let a=9, b=8
+// console.log(a+b)
+// console.log(a*b)
+// let a=9, b=8
+// console.log(Math.pow(a, 2)+Math.pow(b, 2))
+// let a=9, b=8, c=7
+// console.log((a+b+c)/3)
+// let z=9, y=8, x=7
+//
+// console.log(x+1-2*(z-2*x+y));
+// let a=9, x=7
+// console.log(a%3, a%5)
+// console.log(x*1.3, x*2.2)
+// let x=8, y=56
+// console.log(x*0.4, y*0.84)
+// let z=879
+// let suma=z%10
+// z=Math.floor(z/10)
+// suma+=z%10
+// z=Math.floor(z/10)
+// suma+=z%10
+// console.log(suma)
+// let z=564
+// let a=z%10
+// z=Math.floor(z/10)
+// z=Math.floor(z/10)
+// let c=z%10
+// console.log(`${a}0${c}`)
+// let a = 1;
+// let   b = 3;
+// a++ + b;
+// a + ++b;
+// console.log(++a + b++)
+// let years=45, pasport=true
+// years > 18 ? (alert("18+")):(alert("18-") )
+// pasport === true ? alert("pasport - yes") : alert("pasport - no")
+// years > 16 ? (alert("16+")):(alert("16-") )
+// let a=56;
+// a%2===0? alert("u can divide 2") : alert("u cannt divide 2")
